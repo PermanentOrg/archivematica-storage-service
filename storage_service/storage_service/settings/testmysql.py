f3191910f491a5a04ab204250424acaf4d9fcfec
Douglas Cerna <douglascerna@yahoo.com>
2025-03-25 07:51:57 -0600

    Restructure Python project to follow the src layout
    This relocates all the Python code into a new
    src/archivematica/storage_service package and adopts absolute imports.

--- storage_service/storage_service/settings/testmysql.py
+++
@@ -1,8 +0,0 @@
-import dj_database_url
-
-from .test import *
-
-DATABASES["default"] = dj_database_url.parse(
-    "mysql://archivematica:demo@mysql/SSTEST", conn_max_age=600
-)
-DATABASES["default"]["TEST"] = {"NAME": "test_SSTEST"}
