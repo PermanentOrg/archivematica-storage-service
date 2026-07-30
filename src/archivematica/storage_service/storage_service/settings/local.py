ccf76a8e8d0aeff1f695d3c89d345ed651db5578
Liam Lloyd-Tucker <liam@permanent.org>
2026-07-30 16:39:50 -0700

    Set max_conn_age to 0
    Currently, this project keeps its database connections alive for 10
    minutes. Because each call to the dashboard API generates a database
    connection, this can cause the database to run out of available
    connections during periods of heavy upload activity. This commit
    addresses that problem by setting max_conn_age to 0, so database
    connections are not reused at all. This will add some overhead to the
    storage service API, but in the context of processing flows that take
    seconds or minutes that overhead will make only a negligible
    contribution to overall processing times.

--- src/archivematica/storage_service/storage_service/settings/local.py
+++ src/archivematica/storage_service/storage_service/settings/local.py
@@ -11,7 +11,7 @@ from archivematica.storage_service.storage_service.settings.base import *
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
 DATABASES = {}
 if "SS_DB_URL" in environ:
-    DATABASES["default"] = dj_database_url.config(env="SS_DB_URL", conn_max_age=600)
+    DATABASES["default"] = dj_database_url.config(env="SS_DB_URL", conn_max_age=0)
 else:
     DATABASES["default"] = {
         "ENGINE": "django.db.backends.sqlite3",  # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
